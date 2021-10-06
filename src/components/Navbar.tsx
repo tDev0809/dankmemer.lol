98efa47e3af6a9ae6689b37175886df08071c0ef
Illia Butenko <illia.butenko89@gmail.com>
2021-10-06 15:31:29 +0200

    fully responsive navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,4 +1,5 @@
 import Link from "next/link";
+import { useEffect, useState } from "react";
 import { User } from "../types";
 import Dropdown from "./ui/Dropdown";
 
@@ -7,9 +8,19 @@ interface Props {
 }
 
 export default function Navbar({ user }: Props) {
+	const [hamburger, setHamburger] = useState(false);
+
+	useEffect(() => {
+		document.documentElement.style.overflow = hamburger ? "hidden" : "auto";
+	}, [hamburger]);
+
+	useEffect(() => {
+		window.addEventListener("resize", () => setHamburger(false));
+	}, []);
+
 	return (
 		<div className="flex justify-center items-center text-lg">
-			<nav className="max-w-7xl bg-dank-250 rounded-md w-11/12 flex justify-between p-4 mt-5">
+			<nav className="max-w-7xl bg-dank-250 rounded-md flex justify-between p-4 mt-0 lg:mt-5 w-full lg:w-11/12">
 				<div className="flex items-center">
 					<Link href="/">
 						<img
@@ -19,7 +30,7 @@ export default function Navbar({ user }: Props) {
 							width="42"
 						/>
 					</Link>
-					<ul className="ml-5 flex space-x-4">
+					<ul className="ml-5 space-x-4 hidden lg:flex">
 						<li className="inline-block hover:text-dank-50">
 							<Link href="/commands">Commands</Link>
 						</li>
@@ -36,9 +47,12 @@ export default function Navbar({ user }: Props) {
 							<Link href="/feedback">Feedback</Link>
 						</li>
 					</ul>
+					<div className="ml-4 text-xl font-montserrat font-bold inline-block lg:hidden">
+						Dank Memer
+					</div>
 				</div>
 
-				<div className="flex items-center relative">
+				<div className="items-center relative hidden lg:flex">
 					<Link href="https://discord.gg/meme">
 						<a
 							className="inline-block hover:text-dank-50"
@@ -76,33 +90,98 @@ export default function Navbar({ user }: Props) {
 							>
 								<ul className="rounded-md bg-dank-225 mt-2 py-2 text-sm">
 									{(user.isModerator || user.isAdmin) && (
-										<li className="hover:bg-dank-250 w-full px-4 py-1 transition duration-75 ease-in-out">
-											<Link href="/control">
+										<Link href="/control">
+											<li className="hover:bg-dank-250 w-full px-4 py-1 transition duration-75 ease-in-out">
 												Control panel
-											</Link>
-										</li>
+											</li>
+										</Link>
 									)}
-									<li className="hover:bg-dank-250 w-full px-4 py-1 transition duration-75 ease-in-out">
-										<Link href="/appeals">
+									<Link href="/appeals">
+										<li className="hover:bg-dank-250 w-full px-4 py-1 transition duration-75 ease-in-out">
 											Appeal a ban
-										</Link>
-									</li>
-									<li className="hover:bg-dank-250 w-full px-4 py-1 transition duration-75 ease-in-out">
-										<Link href="/reports">
+										</li>
+									</Link>
+									<Link href="/reports">
+										<li className="hover:bg-dank-250 w-full px-4 py-1 transition duration-75 ease-in-out">
 											Report a user
-										</Link>
-									</li>
-									<li className="text-red-400 hover:bg-dank-250 w-full px-4 py-1 transition duration-75 ease-in-out">
-										<Link href="/api/auth/logout">
+										</li>
+									</Link>
+									<Link href="/api/auth/logout">
+										<li className="text-red-400 hover:bg-dank-250 w-full px-4 py-1 transition duration-75 ease-in-out">
 											Logout
-										</Link>
-									</li>
+										</li>
+									</Link>
 								</ul>
 							</Dropdown>
 						</div>
 					)}
 				</div>
+				<div
+					className="items-center relative flex lg:hidden cursor-pointer"
+					onClick={() => setHamburger(!hamburger)}
+				>
+					<span className="material-icons">menu</span>
+				</div>
 			</nav>
+			{hamburger && (
+				<ul className="absolute flex flex-col bg-dank-250 box-border w-screen h-screen z-50 top-[105px]">
+					<Link href="/commands">
+						<li className="hover:text-dank-50 p-4 border-b-2 border-dank-300">
+							Commands
+						</li>
+					</Link>
+					<Link href="/faq">
+						<li className="hover:text-dank-50 p-4 border-b-2 border-dank-300">
+							FAQ
+						</li>
+					</Link>
+					<Link href="/blogs">
+						<li className="hover:text-dank-50 p-4 border-b-2 border-dank-300">
+							Blog
+						</li>
+					</Link>
+					<Link href="/items">
+						<li className="hover:text-dank-50 p-4 border-b-2 border-dank-300">
+							Items
+						</li>
+					</Link>
+					<Link href="/feedback">
+						<li className="hover:text-dank-50 p-4 border-b-2 border-dank-300">
+							Feedback
+						</li>
+					</Link>
+					<Link href="/appeals">
+						<li className="hover:text-dank-50 p-4 border-b-2 border-dank-300">
+							Appeal a ban
+						</li>
+					</Link>
+					<Link href="/reports">
+						<li className="hover:text-dank-50 p-4 border-b-2 border-dank-300">
+							Report a user
+						</li>
+					</Link>
+					{(user?.isModerator || user?.isAdmin) && (
+						<Link href="/control">
+							<li className="hover:text-dank-50 p-4 border-b-2 border-dank-300">
+								Control panel
+							</li>
+						</Link>
+					)}
+					{user ? (
+						<Link href="/reports">
+							<li className="text-red-400 hover:text-red-500 p-4 border-b-2 border-dank-300">
+								Logout
+							</li>
+						</Link>
+					) : (
+						<Link href="/api/auth/logout">
+							<li className="hover:text-dank-50 p-4 border-b-2 border-dank-300">
+								Login
+							</li>
+						</Link>
+					)}
+				</ul>
+			)}
 		</div>
 	);
 }
