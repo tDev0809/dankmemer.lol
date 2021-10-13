2f8edfddd6ab8fb92e53a41234873ecef45cd7e9
Illia Butenko <illia.butenko89@gmail.com>
2021-10-13 23:38:21 +0200

    keys

--- src/pages/collection/[value].tsx
+++ src/pages/collection/[value].tsx
@@ -119,7 +119,7 @@ export default function CollectionPage({ user }: PageProps) {
 			<div className="max-w-4xl xl:max-w-6xl 2xl:max-w-7xl mx-8 lg:mx-auto relative">
 				<div className="grid grid-cols-2 sm:grid-cols-4 xl:grid-cols-6 gap-6 my-16">
 					{collection.map((has, i) => (
-						<div>
+						<div key={i}>
 							<PepeCard index={i + 1} has={has} />
 						</div>
 					))}
