5849ec17fa940d248694cbe1b82622400c8a9e35
Illia Butenko <illia.butenko89@gmail.com>
2021-10-19 23:30:28 +0200

    feedback categories descriptions

--- src/constants.ts
+++ src/constants.ts
@@ -183,6 +183,22 @@ export const FEEDBACK_CATEGORIES = [
 	"other",
 ];
 
+export const FEEDBACK_CATEGORIES_DESCRIPTIONS: Record<string, string> = {
+	currency_items:
+		"Ideas or feedback pertaining to items in Dank Memer's currency",
+	currency_commands:
+		"Ideas or feedback pertaining to commands in Dank Memer's currency system",
+	currency_balances: "Ideas or feedback to balancing Dank Memer's economy",
+	new_features:
+		"Ideas for new features to be added in the bot, can be related to any category",
+	qol_changes: "Ideas and feedback for quality of life changes on dank memer",
+	patreon_and_lootboxes:
+		"Ideas and feedback for any of our premium offerings such as patreon and lootboxes",
+	pipe_dream:
+		"Ideas that you don't think will ever be added, but would be to think about none the less",
+	other: "Any ideas or feedback that don't fit into other categories",
+};
+
 export const REPORTS: Record<string, string[]> = {
 	user: [
 		"Userbot, Autotyper, Macro",
