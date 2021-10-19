cf2c5acd9c9a83dad92c62aadc29376252c4da46
Illia Butenko <illia.butenko89@gmail.com>
2021-10-19 23:51:07 +0200

    staff loading

--- src/pages/staff.tsx
+++ src/pages/staff.tsx
@@ -166,6 +166,12 @@ export default function FeedbackPage({ user }: PageProps) {
 						))}
 					</div>
 				)}
+				{Object.keys(staff).length === 0 && (
+					<div className="mt-16 mb-[1000px] flex flex-col items-center justify-center">
+						<img src="/gif/pepe-dancing.gif" className="h-28" />
+						<div className="font-bold">One moment please...</div>
+					</div>
+				)}
 			</div>
 		</Container>
 	);
