9cad4092740fddd024beecb0007ec194de740e75
Illia Butenko <illia.butenko89@gmail.com>
2021-10-25 00:15:11 +0200

    update blocked message

--- src/components/loot/BlockedCountry.tsx
+++ src/components/loot/BlockedCountry.tsx
@@ -4,7 +4,7 @@ export function BlockedCountry() {
 	return (
 		<div className="mb-40 flex flex-col space-y-2 items-center text-center text-dark-400 dark:text-white">
 			<div className="bg-light-500 dark:bg-dark-400 rounded-md p-20">
-				<div className="text-5xl font-bold">Sorry</div>
+				<div className="text-4xl font-bold">Sorry</div>
 				<div className="flex flex-col items-center space-y-1">
 					<div>
 						Loot boxes are declared illegal in your country. As a
