716f638f2a1177e95f6da2c6d16a23fa38dc4784
Illia Butenko <illia.butenko89@gmail.com>
2021-10-27 10:48:36 +0200

    robots.txt

--- next.config.js
+++ next.config.js
@@ -3,5 +3,13 @@ const path = require("path");
 module.exports = {
 	async redirects() {
 		return [];
-	}
+	},
+	async rewrites() {
+		return [
+			{
+				source: "/robots.txt",
+				destination: "/api/robots",
+			},
+		];
+	},
 };
