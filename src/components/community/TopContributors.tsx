a8ddec86c56cb5e23602d09e4c0c35eace7a966c
Illia Butenko <illia.butenko89@gmail.com>
2021-12-11 15:00:05 +0100

    link to profiles

--- src/components/community/TopContributors.tsx
+++ src/components/community/TopContributors.tsx
@@ -2,6 +2,7 @@ import axios from "axios";
 import { useEffect, useState } from "react";
 import { Contributor } from "../../types";
 import { Avatar } from "../Avatar";
+import Link from "next/link";
 
 export function TopContributors() {
 	const [contributors, setContributors] = useState<Contributor[]>([]);
@@ -15,24 +16,31 @@ export function TopContributors() {
 	return (
 		<div className="bg-dark-100 rounded-md p-4 grid grid-cols-5 justify-items-center">
 			{contributors.map((contributor, i) => (
-				<div className="flex space-x-2 items-center">
-					<Avatar
-						size="48px"
-						id={contributor.id}
-						link={contributor.avatar}
-					/>
-					<div className="flex flex-col -space-y-1">
-						<div className="flex items-end">
-							<div className="font-bold">
-								{contributor.name.replace(/[^\x00-\x7F]/g, "")}
+				<Link href={`/community/profile/${contributor.id}`}>
+					<div className="flex space-x-2 items-center cursor-pointer">
+						<Avatar
+							size="48px"
+							id={contributor.id}
+							link={contributor.avatar}
+						/>
+						<div className="flex flex-col -space-y-1">
+							<div className="flex items-end">
+								<div className="font-bold">
+									{contributor.name.replace(
+										/[^\x00-\x7F]/g,
+										""
+									)}
+								</div>
+								<div className="italic text-light-600 text-sm">
+									#{contributor.discriminator}
+								</div>
 							</div>
-							<div className="italic text-light-600 text-sm">
-								#{contributor.discriminator}
+							<div className="text-sm text-light-600">
+								#{i + 1}
 							</div>
 						</div>
-						<div className="text-sm text-light-600">#{i + 1}</div>
 					</div>
-				</div>
+				</Link>
 			))}
 		</div>
 	);
