f7b50f13c65fc528e2439e25ab6e800edea23f44
Illia Butenko <illia.butenko89@gmail.com>
2021-12-14 12:49:57 -0600

    feat: security headers
    Add a couple headers to all routes in the application to improve application security.

--- next.config.js
+++ next.config.js
@@ -1,6 +1,35 @@
 const path = require("path");
 
 module.exports = {
+	async headers() {
+		return [
+			{
+				source: '/(.*)',
+				headers: [
+					{
+						key: 'X-XSS-Protection',
+						value: '1; mode=block'
+					},
+					{
+						key: 'X-Frame-Options',
+						value: 'SAMEORIGIN'
+					},
+					{
+						key: 'Permissions-Policy',
+						value: 'accelerometer=(), camera=(), geolocation=(), gyroscope=(), magnetometer=(), microphone=(), payment=(), usb=(), interest-cohort=()'
+					},
+					{
+						key: 'X-Content-Type-Options',
+						value: 'nosniff'
+					},
+					{
+						key: 'Referrer-Policy',
+						value: 'origin-when-cross-origin'
+					}
+				]
+			}
+		]
+	},
 	async redirects() {
 		return [
 			{
