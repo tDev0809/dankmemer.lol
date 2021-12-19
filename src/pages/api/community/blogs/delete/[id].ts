fbe4c3126dcb0a933ed776cdb3e5a97d965600e1
Illia Butenko <illia.butenko89@gmail.com>
2021-12-19 16:14:26 +0100

    delete cache on blog deletion

--- src/pages/api/community/blogs/delete/[id].ts
+++ src/pages/api/community/blogs/delete/[id].ts
@@ -1,10 +1,12 @@
 import { NextApiResponse } from "next";
 import { dbConnect } from "../../../../../util/mongodb";
+import { redisConnect } from "../../../../../util/redis";
 import { NextIronRequest, withSession } from "../../../../../util/session";
 
 const handler = async (req: NextIronRequest, res: NextApiResponse) => {
 	const { db } = await dbConnect();
 	const { id } = req.query;
+	const redis = await redisConnect();
 
 	const user = req.session.get("user");
 
@@ -17,6 +19,7 @@ const handler = async (req: NextIronRequest, res: NextApiResponse) => {
 	}
 
 	await db.collection("community-blogs").deleteOne({ _id: id });
+	await redis.del("community:blogs");
 
 	res.status(200).send({});
 };
