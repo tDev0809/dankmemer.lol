9e21c0a343d84777ca95ee442eda22b1ec304d96
Illia Butenko <illia.butenko89@gmail.com>
2021-12-19 17:20:33 +0100

    redirect old blog path to new one

--- next.config.js
+++ next.config.js
@@ -4,37 +4,42 @@ module.exports = {
 	async headers() {
 		return [
 			{
-				source: '/(.*)',
+				source: "/(.*)",
 				headers: [
 					{
-						key: 'X-XSS-Protection',
-						value: '1; mode=block'
+						key: "X-XSS-Protection",
+						value: "1; mode=block",
 					},
 					{
-						key: 'X-Frame-Options',
-						value: 'SAMEORIGIN'
+						key: "X-Frame-Options",
+						value: "SAMEORIGIN",
 					},
 					{
-						key: 'Permissions-Policy',
-						value: 'accelerometer=(), camera=(), geolocation=(), gyroscope=(), magnetometer=(), microphone=(), payment=(), usb=(), interest-cohort=()'
+						key: "Permissions-Policy",
+						value: "accelerometer=(), camera=(), geolocation=(), gyroscope=(), magnetometer=(), microphone=(), payment=(), usb=(), interest-cohort=()",
 					},
 					{
-						key: 'X-Content-Type-Options',
-						value: 'nosniff'
+						key: "X-Content-Type-Options",
+						value: "nosniff",
 					},
 					{
-						key: 'Referrer-Policy',
-						value: 'origin-when-cross-origin'
-					}
-				]
-			}
-		]
+						key: "Referrer-Policy",
+						value: "origin-when-cross-origin",
+					},
+				],
+			},
+		];
 	},
 	async redirects() {
 		return [
 			{
 				source: "/premium",
 				destination: "https://www.patreon.com/join/dankmemerbot",
+				permanent: true,
+			},
+			{
+				source: "/blogs/:path*",
+				destination: `/community/blog/:path*`,
 				permanent: true,
 			},
 		];
