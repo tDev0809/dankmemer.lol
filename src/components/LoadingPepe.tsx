e3af417e7f482956700d52b3fb7ced4022f7a40a
Illia Butenko <illia.butenko89@gmail.com>
2021-12-21 16:50:34 +0100

    fix loading pepe

--- src/components/LoadingPepe.tsx
+++ src/components/LoadingPepe.tsx
@@ -1,6 +1,6 @@
 export default function LoadingPepe() {
 	return (
-		<div className="mt-16 mb-[1000px] flex flex-col items-center justify-center">
+		<div className="mt-16 flex flex-col items-center justify-center">
 			<img src="/gif/pepe-dancing.gif" className="h-28" />
 			<div className="font-bold">One moment please...</div>
 		</div>
