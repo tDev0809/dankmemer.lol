e01a6463be2414eda8c40c4a932fc9f75f251e4f
Illia Butenko <illia.butenko89@gmail.com>
2021-12-30 16:36:23 +0100

    script to fetch recap data

---
+++ scripts/recap-2021.js
@@ -0,0 +1,78 @@
+const { MongoClient, ObjectId } = require("mongodb");
+require("dotenv").config();
+const html2md = require("html-to-md");
+
+const categories = [
+	"currency_items",
+	"currency_commands",
+	"currency_balances",
+	"new_features",
+	"qol_changes",
+	"patreon_and_lootboxes",
+	"website",
+	"pipe_dream",
+	"other",
+];
+
+const data = async () => {
+	const client = await MongoClient.connect(process.env.MONGODB_URI);
+	const db = client.db(process.env.MONGODB_DB);
+
+	const posts = await db.collection("community-posts").find({}).count();
+	const dev = await db
+		.collection("community-posts")
+		.aggregate([
+			{
+				$match: {
+					labels: "developer-response",
+				},
+			},
+		])
+		.toArray();
+	const implemented = await db
+		.collection("community-posts")
+		.aggregate([
+			{
+				$match: {
+					labels: "implemented",
+				},
+			},
+		])
+		.toArray();
+
+	console.log("posts: ", posts);
+	console.log("dev: ", dev.length);
+	console.log("implemented: ", implemented.length);
+	console.log();
+
+	for (const category of categories) {
+		const posts = await db
+			.collection("community-posts")
+			.find({ category })
+			.count();
+
+		const accepted = await db
+			.collection("community-posts")
+			.aggregate([
+				{
+					$match: {
+						category,
+						$or: [
+							{ labels: "accepted" },
+							{ labels: "implemented" },
+						],
+					},
+				},
+			])
+			.toArray();
+
+		console.log("category: ", category);
+		console.log("posts: ", posts);
+		console.log("accepted: ", accepted.length);
+		console.log(" ");
+	}
+
+	process.exit();
+};
+
+data();
