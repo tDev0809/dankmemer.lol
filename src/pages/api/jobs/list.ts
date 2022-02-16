77700a98c0c65df31f7c033b0fbdfb7104cc5cfa
Illia Butenko <illia.butenko89@gmail.com>
2022-02-16 14:58:20 +1000

    Changed requirements for list api jobs

--- src/pages/api/jobs/list.ts
+++ src/pages/api/jobs/list.ts
@@ -6,16 +6,6 @@ import { NextIronRequest, withSession } from "../../../util/session";
 const handler = async (req: NextIronRequest, res: NextApiResponse) => {
 	const db = await dbConnect();
 
-	const user = req.session.get("user");
-
-	if (!user) {
-		return res.status(401).json({ error: "You are not logged in." });
-	}
-
-	if (!user.developer) {
-		return res.status(401).json({ error: "You can't do this." });
-	}
-
 	if (!req.query.team || req.query.team === "all teams") {
 		const jobs = await db.collection("jobs").find({}).toArray();
 		return res.status(200).json(jobs);
